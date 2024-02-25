2003ef49321e1b3de67e1b2227976eeefc529c53
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2024-02-25 18:58:28 -0300

    Corrige nome da columa do tipo de identificador

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS pessoa (
     nome VARCHAR(50) NOT NULL,
     data_nascimento DATE NOT NULL,
     identificador VARCHAR(50) NOT NULL,
-    tipo_identificado VARCHAR(50) NOT NULL,
+    tipo_identificador VARCHAR(50) NOT NULL,
     valor_min_mensal DECIMAL(18,4) NOT NULL,
     valor_max_emprestimo DECIMAL(18,4) NOT NULL
 );
