43de7b243fc7c1f9137dee059a25f8769adf95f0
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2024-02-25 23:12:48 -0300

    Adiciona classe que representa empréstimo

---
+++ src/main/java/br/com/teste/emprestimo/entity/Emprestimo.java
@@ -0,0 +1,40 @@
+package br.com.teste.emprestimo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pessoa")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Emprestimo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "data_criacao")
+  private LocalDate dataCriacao;
+
+  @Column(name = "valor_emprestimo")
+  private BigDecimal valorEmprestimo;
+
+  @Column(name = "numero_parcela")
+  private Integer numeroParcela;
+
+  @Column(name = "status_pagamento")
+  private String statusPagamento;
+
+  @ManyToOne
+  @JoinColumn(name = "pessoa_id", referencedColumnName = "id", nullable = false)
+  @Getter
+  @Setter
+  private Pessoa pessoa;
+}
