d3b2ebfe4a9b2011f7036b37bb3cf897c2c40371
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2024-02-26 07:50:48 -0300

    Adiciona tratativa para quando o JSON estiver incorreto

--- src/main/java/br/com/teste/emprestimo/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/br/com/teste/emprestimo/exception/RestResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -43,9 +44,17 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
   }
 
   @ExceptionHandler({ RegistroDuplicadoException.class })
-  protected  ResponseEntity<Object> handleRegistroDuplicadoException(RegistroDuplicadoException ex) {
+  protected ResponseEntity<Object> handleRegistroDuplicadoException(RegistroDuplicadoException ex) {
     Map<String, String> errors = new HashMap<>();
     errors.put("mensagem", ex.getMessage());
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    Map<String, String> errors = new HashMap<>();
+    errors.put("mensagem", "Algum campo está preenchido incorretamente");
+
     return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 
