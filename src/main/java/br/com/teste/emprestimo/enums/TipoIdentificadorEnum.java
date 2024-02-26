6b9301fdcd00e4a9609f06679f1652448a720a2a
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2024-02-26 07:18:22 -0300

    Remove campo descrição

--- src/main/java/br/com/teste/emprestimo/enums/TipoIdentificadorEnum.java
+++ src/main/java/br/com/teste/emprestimo/enums/TipoIdentificadorEnum.java
@@ -1,21 +1,19 @@
 package br.com.teste.emprestimo.enums;
 
 public enum TipoIdentificadorEnum {
-  PF("PF", 11, "Pessoa Física (PF)", 300.0, 10000.0),
-  PJ("PJ", 14, "Pessoa Jurídica (PJ)", 1000.0, 100000.0),
-  EU("EU", 8, "Estudante Universitário (EU)", 100.0, 10000.0),
-  AP("AP", 10, "Aposentado (AP)", 400.0, 2500000.0);
+  PF("PF", 11, 300.0, 10000.0),
+  PJ("PJ", 14, 1000.0, 100000.0),
+  EU("EU", 8, 100.0, 10000.0),
+  AP("AP", 10, 400.0, 2500000.0);
 
   private String codigo;
   private int tamanho;
-  private String descricao;
   private Double vlrMinMensal;
   private Double vlrMaxEmprestimo;
 
-  TipoIdentificadorEnum(String codigo, int tamanho, String descricao, Double vlrMinMensal, Double vlrMaxEmprestimo) {
+  TipoIdentificadorEnum(String codigo, int tamanho, Double vlrMinMensal, Double vlrMaxEmprestimo) {
     this.codigo = codigo;
     this.tamanho = tamanho;
-    this.descricao = descricao;
     this.vlrMinMensal = vlrMinMensal;
     this.vlrMaxEmprestimo = vlrMaxEmprestimo;
   }
@@ -26,10 +24,6 @@ public enum TipoIdentificadorEnum {
 
   public int getTamanho() {
     return tamanho;
-  }
-
-  public String getDescricao() {
-    return descricao;
   }
 
   public Double getVlrMinMensal() {
