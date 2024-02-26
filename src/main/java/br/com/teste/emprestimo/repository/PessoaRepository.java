cefb74eea74cd0cae4611cc7561aa26ccee236d5
Sérgio Henrique Menta Garcia <garciasergiodm@gmail.com>
2024-02-26 00:40:08 -0300

    Adiciona busca de pessoa pelo identificador

--- src/main/java/br/com/teste/emprestimo/repository/PessoaRepository.java
+++ src/main/java/br/com/teste/emprestimo/repository/PessoaRepository.java
@@ -2,6 +2,9 @@ package br.com.teste.emprestimo.repository;
 
 import br.com.teste.emprestimo.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+  @Query(value = "select * from pessoa where identificador = :identificador", nativeQuery = true)
+  Pessoa findByIdentificador(String identificador);
 }
